beae187fa961f52c74f0f4aedb8d872d76357df0
Wanderson10 <wg21249@gmail.com>
2023-04-25 14:57:35 -0300

    Fix

--- guns_store/settings.py
+++ guns_store/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gunstorebackend-production.up.railway.app','0.0.0.0']
 
 
 # Application definition
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 
 if not DEBUG:
