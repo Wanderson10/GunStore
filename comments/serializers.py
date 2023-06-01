9058c2851e93a89ee14771c5823c7cda2ec81647
Wanderson10 <wg21249@gmail.com>
2023-06-01 18:09:43 -0300

    addExtraFieldsInComments

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,12 +3,14 @@ from .models import Comment
 from users.models import User
 
 class CommentsSerializer(serializers.ModelSerializer):
-  
+    user_name = serializers.SerializerMethodField()
     class Meta:
      model = Comment
-     fields = ["id","comment", "user_comments","commented_at","weapon"]
+     fields = ["id","comment", "user_comments","commented_at","weapon","user_name"]
      read_only_fields = ["id","user_comments", "commented_at"]
 
     
 
      
+    def get_user_name(self, obj:Comment):
+        return  obj.user_comments.username
